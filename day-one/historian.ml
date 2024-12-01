cb34c9f51e9af7090090e5ec8c665b02d2daf266
Safwan Parker <safwan.parker@ingka.com>
2024-12-01 17:14:27 +0100

    d1: part 2

--- day-one/historian.ml
+++ day-one/historian.ml
@@ -1,33 +1,32 @@
 let read_lines () =
-  let rec read_all acc =
-    try
-      let line = read_line () in
-      read_all (line :: acc)
-    with End_of_file -> List.rev acc
-  in
-  read_all []
+    let rec read_all acc =
+      try
+        let line = read_line () in
+        read_all (line :: acc)
+      with End_of_file -> List.rev acc
+    in
+    read_all []
 
 let parse_line line =
   match String.split_on_char ' ' line |> List.filter ((<>) "") with
   | [left; right] -> (int_of_string left, int_of_string right)
   | _ -> failwith "Invalid input line format"
 
-let calculate_total_distance lines =
+let count_occurrences num lst =
+  List.fold_left (fun acc x -> if x = num then acc + 1 else acc) 0 lst
+
+let calculate_similarity_score lines =
   let left_nums, right_nums =
     List.map parse_line lines
     |> List.split
   in
 
-  let sorted_left = List.sort compare left_nums in
-  let sorted_right = List.sort compare right_nums in
-
-  let distances =
-    List.map2 (fun x y -> abs (x - y)) sorted_left sorted_right
-  in
-
-  List.fold_left (+) 0 distances
+  List.fold_left (fun acc num ->
+    let count = count_occurrences num right_nums in
+    acc + (num * count)
+  ) 0 left_nums
 
 let () =
   let lines = read_lines () in
-  let total = calculate_total_distance lines in
-  Printf.printf "Total distance: %d\n" total
+  let similarity = calculate_similarity_score lines in
+  Printf.printf "Similarity score: %d\n" similarity
